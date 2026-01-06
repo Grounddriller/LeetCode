92e62d35fa4b9537da210e1c081debb357e6f52f
Gokhan Yerdelen <yerdelengokhan@gmail.com>
2026-01-06 17:06:41 -0500

    Time: 11 ms (94.97%), Space: 19.4 MB (8.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        maxLength = 0
-        charSet = set()
-        x = 0
+        n = len(s) #length of the string
+        maxLength = 0  #current longest unique substring
+        charSet = set() #stores characters in current window to check of dups
+        x = 0 #left pointer
         
-        for y in range(n):
+        for y in range(n): #y right pointer, moves from left to right accross string
             if s[y] not in charSet:
                 charSet.add(s[y])
                 maxLength = max(maxLength, y - x + 1)
+                #if y[s] is not in window, add to set, compute legth, update max length
             else:
-                while s[y] in charSet:
-                    charSet.remove(s[x])
-                    x += 1
-                charSet.add(s[y])
+                while s[y] in charSet: #move left side foward until dup character is gone
+                    charSet.remove(s[x]) #remove the character at left edge
+                    x += 1 #move left edge right by 1
+                charSet.add(s[y]) #adds to set once dup is gone
         
-        return maxLength
-            
+        return maxLength #Returns longest unique string
+
+#len(s): returns number of characters
+#set(): creates and empty set(an unordered collection of unique, immutable elements)
+#s[x] checks left most character
+#s[y] checks rightmost character
