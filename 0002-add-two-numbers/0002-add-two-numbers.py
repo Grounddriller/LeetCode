44f8ffab45a481bc1e7a8a263cf0117e872d8c71
Gokhan Yerdelen <yerdelengokhan@gmail.com>
2026-01-06 16:53:25 -0500

    Time: 0 ms (100%), Space: 19.4 MB (9.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,18 +7,20 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        dummyHead = ListNode(0)
-        curr = dummyHead
-        carry = 0
-        while l1 != None or l2 != None or carry != 0:
-            l1Val = l1.val if l1 else 0
-            l2Val = l2.val if l2 else 0
-            columnSum = l1Val + l2Val + carry
-            carry = columnSum // 10
-            newNode = ListNode(columnSum % 10)
-            curr.next = newNode
-            curr = newNode
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return dummyHead.next
-        
+        dummyHead = ListNode(0) #False starting node
+        curr = dummyHead #pointer to the lastest node
+        carry = 0 #Stores carry values
+        while l1 != None or l2 != None or carry != 0: #makes sure to go through every digit in the lists
+            l1Val = l1.val if l1 else 0 #If the list still has a node, use its digit, else treat it like digit 0.
+            l2Val = l2.val if l2 else 0 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            columnSum = l1Val + l2Val + carry #ones column sum
+            carry = columnSum // 10 #next digit carry
+            newNode = ListNode(columnSum % 10) #digit stored in the current output node
+            curr.next = newNode #appends new node to the result list
+            curr = newNode #moves curr foward in the list fo we can append the next digit
+            l1 = l1.next if l1 else None #move to the next node if l1 exists 
+            l2 = l2.next if l2 else None #move to the next node if l2 exists 
+        return dummyHead.next #Ruturns final results, skipping dummyHeada
+
+# sum % 10: gives the last digit of a number.
+# sum // 10: gives how many tens are in the number — i.e. the carry.
