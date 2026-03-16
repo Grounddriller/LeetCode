fae3d3d40ec13882966511b68e2383fa75a8a1ac
Gokhan Yerdelen <yerdelengokhan@gmail.com>
2026-03-16 15:56:11 -0400

    Add max_profit function for stock trading analysis
    Implement max_profit function to calculate maximum profit from stock prices.

---
+++ grokking-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,16 @@
+def max_profit(prices):
+    left = 0 # buy day
+    right = 0 # sell day
+    max_profit = 0
+    
+    while right < len(prices):
+        if prices[right] > prices[left]:
+            profit = prices[right] - prices[left]
+            max_profit = max(max_profit, profit)
+            
+        else:
+            left = right
+            
+        right += 1
+        
+    return max_profit
