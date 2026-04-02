40104627c22a01ea19b23f064f62ecd8dd8cc803
Gokhan Yerdelen <yerdelengokhan@gmail.com>
2026-04-02 17:29:24 -0400

    Implement function to check for nearby duplicates

---
+++ grokking-contains-duplicate-II.py
@@ -0,0 +1,13 @@
+def contains_nearby_duplicate(nums, k):
+    window = set()
+
+    for i in range(len(nums)):
+        if nums[i] in window:
+            return True
+
+        window.add(nums[i])
+
+        if len(window) > k:
+            window.remove(nums[i - k])
+
+    return False
