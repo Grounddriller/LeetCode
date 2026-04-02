6e5a4a55780a8b539baca01a2f9dc1d7042d09ae
Gokhan Yerdelen <yerdelengokhan@gmail.com>
2026-04-02 17:25:55 -0400

    Implement climbing stairs function

---
+++ grokking-climbing-stairs.py
@@ -0,0 +1,13 @@
+def climb_stairs(n):
+    if n <= 2:
+        return n
+
+    prev2 = 1
+    prev1 = 2
+
+    for _ in range(3, n + 1):
+        curr = prev1 + prev2
+        prev2 = prev1
+        prev1 = curr
+
+    return prev1
