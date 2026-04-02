89ec621a91dc440bc34934d2900754418df3362b
Gokhan Yerdelen <yerdelengokhan@gmail.com>
2026-04-02 17:51:57 -0400

    Create grokking-find-if-path-exsits-in-graph.py

---
+++ grokking-find-if-path-exsits-in-graph.py
@@ -0,0 +1,28 @@
+def valid_path(n, edges, source, destination):
+    parent = list(range(n))
+    rank = [0] * n
+
+    def find(x):
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
+
+    def union(x, y):
+        root_x = find(x)
+        root_y = find(y)
+
+        if root_x == root_y:
+            return
+
+        if rank[root_x] < rank[root_y]:
+            parent[root_x] = root_y
+        elif rank[root_x] > rank[root_y]:
+            parent[root_y] = root_x
+        else:
+            parent[root_y] = root_x
+            rank[root_x] += 1
+
+    for u, v in edges:
+        union(u, v)
+
+    return find(source) == find(destination)
